dad1e49e8069673e9512c99e1ff5c6d1baba42e7
Matt Travi <programmer@travi.org>
2020-04-24 21:58:17 -0500

    feat(test): verified that a js project has an .nvmrc file

--- src/test-test.js
+++ src/test-test.js
@@ -1,11 +1,53 @@
+import {promises as fs} from 'fs';
+import sinon from 'sinon';
+import {assert} from 'chai';
+import any from '@travi/any';
 import testApplicability from './test';
 
 suite('applicability test', () => {
+  let sandbox;
+
+  setup(() => {
+    sandbox = sinon.createSandbox();
+
+    sandbox.stub(fs, 'stat');
+  });
+
+  teardown(() => sandbox.restore());
+
   test('that `true` is returned if the project has an `.nvmrc` file', async () => {
-    await testApplicability();
+    const projectRoot = any.string();
+    fs.stat.withArgs(`${projectRoot}/.nvmrc`).returns({isFile: () => true});
+
+    assert.isTrue(await testApplicability({projectRoot}));
+  });
+
+  test('that `false` is returned if the project\'s `.nvmrc` is not file', async () => {
+    const projectRoot = any.string();
+    fs.stat.withArgs(`${projectRoot}/.nvmrc`).returns({isFile: () => false});
+
+    assert.isFalse(await testApplicability({projectRoot}));
   });
 
   test('that `false` is returned if the project does not have an `.nvmrc` file', async () => {
+    const error = new Error();
+    error.code = 'ENOENT';
+    fs.stat.rejects(error);
 
+    assert.isFalse(await testApplicability({}));
+  });
+
+  test('that errors other than for missing file', async () => {
+    const error = new Error();
+    error.code = any.word();
+    fs.stat.rejects(error);
+
+    try {
+      await testApplicability({});
+
+      throw new Error('an error should have been thrown by the module under test');
+    } catch (e) {
+      assert.equal(e, error);
+    }
   });
 });
--- src/test.js
+++ src/test.js
@@ -1,3 +1,13 @@
-export default function () {
+import {promises as fs} from 'fs';
 
+export default async function ({projectRoot}) {
+  try {
+    const stats = await fs.stat(`${projectRoot}/.nvmrc`);
+
+    return stats.isFile();
+  } catch (e) {
+    if ('ENOENT' === e.code) return false;
+
+    throw e;
+  }
 }
