a9bfeb2f9451b651d6d09cc91d14aed281069223
Matt Travi <programmer@travi.org>
2020-04-27 22:58:20 -0500

    feat(output): provided output about lifting steps

--- src/lift.js
+++ src/lift.js
@@ -1,5 +1,8 @@
+import {info} from '@travi/cli-messages';
 import liftScripts from './scripts';
 
 export default async function ({results, projectRoot}) {
+  info('Lifting JavaScript-specific details');
+
   await liftScripts({projectRoot, scripts: results.scripts});
 }
--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,10 @@
 import {promises as fs} from 'fs';
+import {info} from '@travi/cli-messages';
 
 export default async function ({projectRoot, scripts}) {
   if (scripts) {
+    info('Adding npm scripts to `package.json`', {level: 'secondary'});
+
     const pathToPackageJson = `${projectRoot}/package.json`;
 
     const existingPackageJsonContents = JSON.parse(await fs.readFile(pathToPackageJson, 'utf8'));
