5db14e7c84ebecd2f5537ea1a4b095dbd42cba95
Matt Travi <programmer@travi.org>
2020-05-10 16:50:10 -0500

    test(integration): ignored import rule before preview creates link

--- test/integration/features/step_definitions/common-steps.js
+++ test/integration/features/step_definitions/common-steps.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+// eslint-disable-next-line import/no-extraneous-dependencies,import/no-unresolved
 import {lift} from '@form8ion/lift-javascript';
 import stubbedFs from 'mock-fs';
 import any from '@travi/any';
