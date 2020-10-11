755f8a7f8579bd0abfe7c74cffe0443e0abd3050
Matt Travi <programmer@travi.org>
2020-10-11 09:56:55 -0500

    test(node-modules-stub): made progress decoupling from the structure of the dependency tree
    but havent been successful decoupling from the pnpm structure yet

--- test/integration/features/step_definitions/common-steps.js
+++ test/integration/features/step_definitions/common-steps.js
@@ -1,13 +1,13 @@
 // eslint-disable-next-line import/no-extraneous-dependencies,import/no-unresolved
 import {lift} from '@form8ion/lift-javascript';
 import {resolve} from 'path';
-import {promises as fs} from 'fs';
 import stubbedFs from 'mock-fs';
 import any from '@travi/any';
 import {After, Before, When} from 'cucumber';
 
 const packagePreviewDirectory = '../__package_previews__/lift-javascript';
 const pathToNodeModules = [__dirname, '../../../../', 'node_modules/'];
+const stubbedNodeModules = stubbedFs.load(resolve(...pathToNodeModules));
 
 Before(async function () {
   this.existingScripts = any.simpleObject();
@@ -26,20 +26,11 @@ When('the scaffolder results are processed', async function () {
     [packagePreviewDirectory]: {
       '@form8ion': {
         'lift-javascript': {
+          // node_modules: stubbedNodeModules,
           node_modules: {
             '.pnpm': {
-              node_modules: {
-                'color-name': {'index.js': await fs.readFile(resolve(...pathToNodeModules, 'color-name/index.js'))}
-              },
-              'ansi-styles@4.3.0': {
-                node_modules: {
-                  'color-convert': {
-                    'index.js': await fs.readFile(resolve(...pathToNodeModules, 'color-convert/index.js')),
-                    'conversions.js': await fs.readFile(resolve(...pathToNodeModules, 'color-convert/conversions.js')),
-                    'route.js': await fs.readFile(resolve(...pathToNodeModules, 'color-convert/route.js'))
-                  }
-                }
-              }
+              node_modules: stubbedNodeModules,
+              'ansi-styles@4.3.0': {node_modules: stubbedNodeModules}
             }
           }
         }
